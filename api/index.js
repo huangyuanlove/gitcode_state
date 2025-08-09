28020a89ad6b883e56d39d8aa448853af4ea0549
huangyuan <huangyuan@huangyuanlove.com>
2025-08-09 13:03:04 +0800

    add express

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
-module.exports = (req, res) => {
-  res.status(200).json({ 
-    toc: 'index'
-  });
-};
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 中间件
+app.use(express.json());
+
+// 测试路由
+app.get('/', (req, res) => {
+  res.send('Node项目已启动！');
+});
