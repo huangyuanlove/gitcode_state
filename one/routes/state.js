dbd0f0591b47de8d5d74e8240120cd5b001b776c
huangyuan <huangyuan@huangyuanlove.com>
2025-08-10 21:52:00 +0800

    format

--- one/routes/state.js
+++ one/routes/state.js
@@ -1,8 +1,6 @@
 
 import express from 'express'
-
 import { renderStatsCard } from './cards/stats-card.js'
-import Card from './cards/common/Card.js';
 import {
   CustomError,
   clampValue,
@@ -25,10 +23,10 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
 
   res.setHeader("Content-Type", "image/svg+xml");
-  // res.setHeader(
-  //   "Cache-Control",
-  //   `max-age=10, s-maxage=10, stale-while-revalidate=86400`,
-  // );
+  res.setHeader(
+    "Cache-Control",
+    `max-age=10, s-maxage=10, stale-while-revalidate=86400`,
+  );
 
 
 
@@ -78,22 +76,22 @@ router.get('/', function (req, res, next) {
   };
 
 
-const showStats = parseArray(show);
+  const showStats = parseArray(show);
 
 
-    let cacheSeconds = clampValue(
-      parseInt(cache_seconds || CONSTANTS.CARD_CACHE_SECONDS, 10),
-      CONSTANTS.TWELVE_HOURS,
-      CONSTANTS.TWO_DAY,
-    );
-    cacheSeconds = process.env.CACHE_SECONDS
-      ? parseInt(process.env.CACHE_SECONDS, 10) || cacheSeconds
-      : cacheSeconds;
+  let cacheSeconds = clampValue(
+    parseInt(cache_seconds || CONSTANTS.CARD_CACHE_SECONDS, 10),
+    CONSTANTS.TWELVE_HOURS,
+    CONSTANTS.TWO_DAY,
+  );
+  cacheSeconds = process.env.CACHE_SECONDS
+    ? parseInt(process.env.CACHE_SECONDS, 10) || cacheSeconds
+    : cacheSeconds;
 
-    // res.setHeader(
-    //   "Cache-Control",
-    //   `max-age=${cacheSeconds}, s-maxage=${cacheSeconds}, stale-while-revalidate=${CONSTANTS.ONE_DAY}`,
-    // );
+  res.setHeader(
+    "Cache-Control",
+    `max-age=${cacheSeconds}, s-maxage=${cacheSeconds}, stale-while-revalidate=${CONSTANTS.ONE_DAY}`,
+  );
 
 
   const { titleColor, iconColor, textColor, bgColor, borderColor, ringColor } =
@@ -107,53 +105,53 @@ const showStats = parseArray(show);
       theme,
     });
 
-console.error(`getCardColors result,title_color=${title_color},text_color=${text_color},icon_color=${icon_color},bg_color=${bg_color},border_color=${border_color},ring_color=${ring_color},theme=${theme}`)
+  console.error(`getCardColors result,title_color=${title_color},text_color=${text_color},icon_color=${icon_color},bg_color=${bg_color},border_color=${border_color},ring_color=${ring_color},theme=${theme}`)
 
 
   try {
 
-    let result =       renderStatsCard(stats, {
-        hide: parseArray(hide),
-        show_icons: parseBoolean(show_icons),
-        hide_title: parseBoolean(hide_title),
-        hide_border: parseBoolean(hide_border),
-        card_width: parseInt(card_width, 10),
-        hide_rank: parseBoolean(hide_rank),
-        include_all_commits: parseBoolean(include_all_commits),
-        line_height,
-        title_color,
-        ring_color,
-        icon_color,
-        text_color,
-        text_bold: parseBoolean(text_bold),
-        bg_color,
-        theme,
-        custom_title,
-        border_radius,
-        border_color,
-        number_format,
-        locale: locale ? locale.toLowerCase() : null,
-        disable_animations: parseBoolean(disable_animations),
-        rank_icon,
-        show: showStats,
-      });
+    let result = renderStatsCard(stats, {
+      hide: parseArray(hide),
+      show_icons: parseBoolean(show_icons),
+      hide_title: parseBoolean(hide_title),
+      hide_border: parseBoolean(hide_border),
+      card_width: parseInt(card_width, 10),
+      hide_rank: parseBoolean(hide_rank),
+      include_all_commits: parseBoolean(include_all_commits),
+      line_height,
+      title_color,
+      ring_color,
+      icon_color,
+      text_color,
+      text_bold: parseBoolean(text_bold),
+      bg_color,
+      theme,
+      custom_title,
+      border_radius,
+      border_color,
+      number_format,
+      locale: locale ? locale.toLowerCase() : null,
+      disable_animations: parseBoolean(disable_animations),
+      rank_icon,
+      show: showStats,
+    });
 
 
 
 
-  
+
     res.send(
- result
+      result
     );
   } catch (err) {
     console.log("res.send error")
-     console.log(err.message)
-      console.log( err.secondaryMessage)
-    // res.setHeader(
-    //   "Cache-Control",
-    //   `max-age=${CONSTANTS.ERROR_CACHE_SECONDS / 2}, s-maxage=${CONSTANTS.ERROR_CACHE_SECONDS
-    //   }, stale-while-revalidate=${CONSTANTS.ONE_DAY}`,
-    // ); 
+    console.log(err.message)
+    console.log(err.secondaryMessage)
+    res.setHeader(
+      "Cache-Control",
+      `max-age=${CONSTANTS.ERROR_CACHE_SECONDS / 2}, s-maxage=${CONSTANTS.ERROR_CACHE_SECONDS
+      }, stale-while-revalidate=${CONSTANTS.ONE_DAY}`,
+    );
     return res.send(
       renderError(err.message, err.secondaryMessage, {
         title_color,
